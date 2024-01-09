1f52b1c0ada8a2f099aae61a34ebbf8dc1e891bc
Noctiscreo <noctiscreo@gmail.com>
2024-01-09 14:56:02 +0000

    Added SQLAlchemy.

--- flaskblog.py
+++ flaskblog.py
@@ -1,8 +1,10 @@
+from datetime import datetime
+
 # render_template enalbes Flask to render html files.
 # url_for enables linking to files, e.g. for CSS files:
 # <link rel="stylesheet" type="text/css" href="{{ url_for('static', filename='main.css') }}">
 from flask import Flask, render_template, url_for, flash, redirect
-
+from flask_sqlalchemy import SQLAlchemy
 # Import the forms you've made in forms.py:
 from forms import RegistrationForm, LoginForm
 
@@ -11,6 +13,53 @@ app = Flask(__name__)
 
 # Sets up a 'secret key' for WTForms
 app.config['SECRET_KEY'] = 'b8453cb5dd480b2a768f4ae0acc3eaa0'
+# /// = relative path from the current file.
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
+db = SQLAlchemy(app)
+
+# Each class is a table in the database:
+# Imports from db.Model.
+class User(db.Model):
+    # Columns for each table (primary key = unique ID for the user):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    # The password will be a hashed string, 60 characters long.
+    password = db.Column(db.String(60), nullable=False)
+    
+    # Establish relationship to other tables.
+    # Does not create a column in the database, it only makes a query.
+    # The User table is related to the Post table.
+    # 'Post' references the Post class.
+    # 'author' gets the user who created the post.
+    # lazy means SQLAlchemy will load the data as necessary in one go,
+    # i.e. all of the posts created by a user.
+    posts = db.relationship('Post', backref='author', lazy=True)
+
+    # Specifies an __repr__ method (magic method / dunderscore method).
+    def __repr__ (self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+    
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    # Be sure to import datetime from datetime.
+    # utcnow is needed to keep dates and times consistent.
+    # No parentheses for utcnow because you don't want the current time now.
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    # Grabs the primary key of the user.
+    # 'user.id' references the user table name, and id column.
+    # User class automatically sets its table name to 'user'.
+    # Table names can be changed if needed.
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__ (self):
+        return f"Post('{self.title}', '{self.date_posted}')"
+    
+
 
 posts = [
     {
